429c9744bf7161da94560ddc723dfedec5ba462b
Emre derman <6emrederman@gmail.com>
2021-10-26 23:33:53 +0300

    .

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import sys
 # FileDownloader <index_file> [<lower_endpoint>-<upper_endpoint>]
 """
 <index file>: [Required] The URL of the index that includes a list of text file URLs.
-
 <lower endpoint>-<upper endpoint>: [Optional] If this argument is not given, a file in the index is downloaded if it is found in the index. 
 Otherwise, the bytes between <lower endpoint> and <upper endpoint> inclusively are to be downloaded.
-
 """
 
+default_string_filler = "Not Available"
 class FileDownloader:
     def __init__(self,arguments):
+        self.lower_endpoint = default_string_filler
+        self.upper_endpoint = default_string_filler
         self.target_url = arguments[0]
-
-        if (arguments[1] != None ):
+        if (len(arguments) > 1):
             self.lower_endpoint = arguments[1][:arguments[1].find("-")]
             self.upper_endpoint = arguments[1][arguments[1].rfind("-") + 1:]
             self.option = 1
@@ -23,10 +23,14 @@ class FileDownloader:
             # Option 0 means no boundary
             # Option 1 means boundary
             self.option = 0
+        print(" self.lower : " + self.lower_endpoint+ "\n"
+              " self.upper : " + self.upper_endpoint+ "\n"
+              " self.target : " + self.target_url+ "\n"
+              " self.option : " + str(self.option))
+
 
     def downloadFile(self):
         response = urllib.request.urlopen(self.target_url)
-
         if(response == "200 OK"):
             data = response.read()  # a `bytes` object
             while(data):
