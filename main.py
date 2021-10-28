3c577e9a29a9281e9f5127732578ed1b2c2b47b5
Emre derman <6emrederman@gmail.com>
2021-10-29 01:01:55 +0300

    the socket has successfully connected.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 
+
 # Input type
 # FileDownloader <index_file> [<lower_endpoint>-<upper_endpoint>]
 """
@@ -16,7 +17,7 @@ class FileDownloader:
         self.lower_endpoint = default_string_filler
         self.upper_endpoint = default_string_filler
         self.target_url = arguments[0]
-        self.port = 80
+        self.target_port = 80
         if (len(arguments) > 1):
             self.lower_endpoint = arguments[1][:arguments[1].find("-")]
             self.upper_endpoint = arguments[1][arguments[1].rfind("-") + 1:]
@@ -29,44 +30,6 @@ class FileDownloader:
               " self.upper : " + self.upper_endpoint+ "\n"
               " self.target : " + self.target_url+ "\n"
               " self.option : " + str(self.option))
-
-
-    def send_request(self,target_url):
-
-        # Socket programming
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # connect the client
-        client.connect((target_url, self.target_port))
-
-        # send some data
-        request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % target_url
-        client.send(request.encode())
-
-        # receive some data
-        response = client.recv(4096)
-        http_response = repr(response)
-        http_response_len = len(http_response)
-
-    def example_request(self):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print("Socket successfully created")
-        except socket.error as err:
-            print("socket creation failed with error %s" % (err))
-            sys.exit()
-
-        # default port for socket
-        try:
-            host_ip = socket.gethostbyname(self.target_url)
-            print("Host IP is : "+host_ip)
-        except socket.gaierror:
-            # this means could not resolve the host
-            print("there was an error resolving the host")
-            sys.exit()
-
-        # connecting to the server
-        s.connect((host_ip, self.port))
-        print("the socket has successfully connected.")
 
     def post_request(self):
         headers = """GET {} HTTP/1.1
@@ -84,15 +47,29 @@ class FileDownloader:
     def __str__(self):
         return print(self.target_url +"\n"+ self.lower_endpoint + "\n" + self.upper_endpoint)
 
+
+    def send_request(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Socket successfully created")
+        except socket.error as err:
+            print("socket creation failed with error %s" % (err))
+            sys.exit()
+        # default port for socket
+        try:
+            host_ip = socket.gethostbyname(self.target_url[:self.target_url.find("/")])
+            print("Host IP is : " + host_ip)
+        except socket.gaierror:
+            # this means could not resolve the host
+            print("there was an error resolving the host")
+            sys.exit()
+        # connecting to the server
+        s.connect((host_ip, self.target_port))
+        print("the socket has successfully connected.")
+
+
 arguments = sys.argv
 arguments = arguments[1:]
 file_ptr = FileDownloader(arguments)
-file_ptr.example_request()
+file_ptr.send_request()
 
-
-
-"""
-RESOURCES
-
-https://www.codestudyblog.com/cnb/0624123507.html
-"""
