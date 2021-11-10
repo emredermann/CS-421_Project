5fd79eef6fb827de787e2aa3a48dcf76d553b130
Emre derman <6emrederman@gmail.com>
2021-11-10 15:23:45 +0300

    FINITO.

--- FileDownloader.py
+++ FileDownloader.py
@@ -13,20 +13,7 @@ def get_request_msg(target_download_url: str, request_type="GET", custom_header=
     msg = f'{request_type} /{target_download_url[target_download_url.find("/"):]} HTTP/1.1\r\nHost:%s\r\n\r\n'%target_download_url[:target_download_url.find("/")]
     return msg
 
-# def make_request(filename,range):
-#     range_header = "Range: bytes=0-{}".format(range)
-#     message = get_request_msg(
-#         filename, request_type="HEAD", custom_header=range_header)
-#     print("message is : " + message)
-#     s.sendall(message.encode())
-#     response = s.recv(500)
-#     with open(filename, 'w') as file:
-#         file.write(response.decode())
-#     return response.decode()
-
-
-default_string_filler = "Not Available"
-print("program has been started")
+print("Program has been started")
 arguments = sys.argv
 arguments = arguments[1:]
 range_is_given = False
@@ -40,17 +27,16 @@ else:
     lower_endpoint = 0
     upper_endpoint = 999
 
-
 target_url = arguments[0]
 file_name = target_url[target_url.rfind("/")+1:]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-print("Target url is : "+ target_url)
+# print("Target url is : "+ target_url)
 server_hostIP = socket.gethostbyname(target_url[:target_url.find("/")])
 
 # server_hostIP = socket.gethostbyname('google.com')
-print("server_hostIP : "+server_hostIP)
+# print("server_hostIP : "+server_hostIP)
 
 server_port = 80
 
@@ -70,48 +56,75 @@ print(f'Connected to {server_hostIP} on {server_port} port.')
 range_header = "Range: bytes = 0-1024"
 msg = get_request_msg(target_url, request_type="GET", custom_header = range_header)
 print('Sending request...')
-print("Message is : " + msg)
-s.sendall(msg.encode())
-response1 = s.recv(BUFFER_SIZE)
-# response1 = response1[response1.rfind("\r"):]
-url_list = response1.decode().split("\n")
-url_list = url_list[url_list.index('\r')+1:-1]
+# print("Message is : " + msg)
+
+try:
+    s.sendall(msg.encode())
+    response = s.recv(BUFFER_SIZE)
+    response1 = response.decode()
+    url_list = response1.split("\n")
+    with open(target_url[target_url.rfind('/') + 1:], 'wb') as file:
+        file.write(response)
+    print( target_url[target_url.rfind('/') + 1:] + " is downloaded.")
+    url_list = url_list[url_list.index('\r')+1:-1]
+except:
+    print(f"{target_url} could not founded ...")
+    print("Program will exit.")
+    sys.exit(1)
+
 print(url_list)
-print("number of file URLs in the index file : "+ str(len(url_list)))
+print(f"There are {len(url_list)} files in the index. ")
+counter = 1
 for x in url_list:
-        print("X is " + x)
+        # print("X is " + x)
         # Request type changed to HEAD
         msg = get_request_msg(x, request_type="HEAD", custom_header=range_header)
-        print('Sending head request...')
-        print("Message is : " + msg)
+        # print('Sending head request...')
+        # print("Message is : " + msg)
         s.sendall(msg.encode())
         response = s.recv(BUFFER_SIZE)
-        if response.decode().find("<h1>Not Found</h1>")>0:
-            print("File not Found...")
-            pass
+        response = response.decode()
+        response1 = response.split("\n")
+        tmp = (response1[-4].split(" "))
+        content_length = 0
+        if len(tmp) > 1:
+            if tmp[0] == "Content-Length":
+                content_length = tmp[1]
+                print(f'{tmp[0]} is : ' + tmp[1])
+        if response1[0] == ('HTTP/1.1 404 Not Found\r'):
+            print(str(counter)+" " + f"{x}  not found...")
         else:
             if(range_is_given == False):
                 msg = get_request_msg(x, request_type="GET", custom_header=range_header)
-                print('Sending get request...')
+                #print('Sending get request...')
                 s.sendall(msg.encode())
                 response = s.recv(BUFFER_SIZE)
                 data = response.decode()
-                with open(x[x.rfind('/')+1:], 'wb') as file:
-                    file.write(response)
-            elif(lower_endpoint > msg.__sizeof__()):
-                print("lower_endpoint > response.__sizeof__()"
-                      "The file could not downloaded.")
+                response1 = data.split("\n")
+                if response1[0] == ('HTTP/1.1 404 Not Found\r'):
+                    print(str(counter)+" " + f"{x}  not found...")
+                else:
+                    with open(x[x.rfind('/')+1:], 'wb') as file:
+                        file.write(response)
+                    print(str(counter)+" " + x[x.rfind('/')+1:] + range_header + " is downloaded")
+            elif(lower_endpoint > content_length):
+                print(str(counter) +
+                      f"{x} could not downloaded."+ "lower_endpoint > response.__sizeof__()")
                 pass
-            #obtain a part of the file content from the HTTP 206 Partial Content response.
-            elif(lower_endpoint <= msg.__sizeof__()):
+            elif(lower_endpoint <= content_length):
                 local_range_header = f"Range: bytes = {lower_endpoint}-{upper_endpoint}"
                 msg = get_request_msg(x, request_type="GET", custom_header=local_range_header)
-                print('Sending get request...')
+                #print('Sending get request...')
                 s.sendall(msg.encode())
                 response = s.recv(BUFFER_SIZE)
                 data = response.decode()
-                with open(x[x.rfind('/')+1:], 'wb') as file:
-                    file.write(response)
-        print("******************************* \n \n")
+                response1 = data.split("\n")
+                if response1[0] == ('HTTP/1.1 404 Not Found\r'):
+                    print(str(counter)+" " +f"{x} not Found...")
+                else:
+                    with open(x[x.rfind('/')+1:], 'wb') as file:
+                        file.write(response)
+                    print(str(counter) +" "+ x[x.rfind('/')+1:] + local_range_header + " is downloaded")
+        counter += 1
 s.close()
 print('Connection was closed.')
